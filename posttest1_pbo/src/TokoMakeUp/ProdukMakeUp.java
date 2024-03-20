bc85625173a181003079890d64a79bcc1e9f7df7
AJIDINDANR <145553085+AJIDINDANR@users.noreply.github.com>
2024-03-20 17:28:24 +0800

    Add files via upload
    POSTTEST 2

--- posttest1_pbo/src/TokoMakeUp/ProdukMakeUp.java
+++ posttest1_pbo/src/TokoMakeUp/ProdukMakeUp.java
@@ -9,18 +9,22 @@ public class ProdukMakeUp {
         this.harga = harga;
     }
 
+    // Getter untuk nama produk
     public String getNamaProduk() {
         return namaProduk;
     }
 
+    // Setter untuk nama produk
     public void setNamaProduk(String namaProduk) {
         this.namaProduk = namaProduk;
     }
 
+    // Getter untuk harga
     public double getHarga() {
         return harga;
     }
 
+    // Setter untuk harga
     public void setHarga(double harga) {
         this.harga = harga;
     }
